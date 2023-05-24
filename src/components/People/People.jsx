dfc89483c3552666eb31f80099c9b4c9024de101
OFFME DoD <offmemyg@gmail.com>
2023-05-24 19:56:09 +0300

    axios start

--- src/components/People/People.jsx
+++ src/components/People/People.jsx
@@ -1,22 +1,30 @@
 import cla from './People.module.css';
-import React from 'react';
+import axios from 'axios';
+import userPgoto from '../../asses/images/usersimg.jpg';
 let People = (props) => {
+
+if(props.peoplelist.length === 0){
+    axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response =>{
+    props.setusers(response.data.items);
+    })
+}
+
 
     return <div className={cla.container}>
         {
- 
-            props.peoplelist.map(u=><div key={u.id}>
-                <div className={cla.block}>
+            props.peoplelist.map(u=><div className={cla.block} key={u.id}>
+                <div className={cla.ava__desp}>
                     <div className={cla.user__photo}>
-                    <img src={u.avauser} />
+                    <img  src={u.photos.small != null ? u.photos.small: userPgoto} alt='f' />
                     </div>
-                    <div>
+                    <div className={cla.name__status}>
                 <div>{u.name}</div>
-                <div>{u.status}</div>
-                   <div>{u.followed ?  <button onClick={()=>{props.unfollow(u.id)}}>Unfollow</button>
-                   : <button onClick={()=>{props.follow(u.id)}}>Follow</button>}</div> 
-               
+                <span>{"u.location.sity"} , {"u.location.country"}</span>
                 </div>
+                </div> 
+                <div>
+                   <div>{u.followed ? <button onClick={()=>{props.unfollow(u.id)}}>UNFOLLOW</button>
+                                    : <button onClick={()=>{props.follow(u.id)}}>FOLLOWт</button>}</div>             
                 </div>
                 
             </div>)
--- src/redux/people-reducer.js
+++ src/redux/people-reducer.js
@@ -3,26 +3,7 @@ const UNFOLLOW = "UNFOLLOW";
 const USERS = "USERS";
 
 let initialState = {
-  PeopleData: [
-    {
-      id: 0,
-      followed: true,
-      avauser:
-        "https://cdn2.iconfinder.com/data/icons/metaverse-12/512/metaverse_vr_virtual_reality_avatar_headset_male-256.png",
-      name: "Denni",
-      status: "О heh eye",
-      location: { sity: "Kazan", country: "Russia" },
-    },
-    {
-      id: 1,
-      followed: false,
-      avauser:
-        "https://cdn4.iconfinder.com/data/icons/people-avatar-filled-outline/64/girl_female_young_people_woman_teenager_avatar-256.png",
-      name: "Jean",
-      status: "I like capybara",
-      location: { sity: "Kazan", country: "Russia" },
-    },
-  ],
+  PeopleData: [],
 };
 
 const peoplereduce = (state = initialState, action) => {
