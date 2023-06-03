00d9debf01bbaddb07e4b10c8a06133e0b395464
OFFME DoD <offmemyg@gmail.com>
2023-06-03 22:52:58 +0300

    login add redux-form

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,16 +1,22 @@
-import { applyMiddleware, combineReducers, legacy_createStore as createStore } from "redux";
+import {
+  applyMiddleware,
+  combineReducers,
+  legacy_createStore as createStore,
+} from "redux";
 import profilereduce from "./profile-reducer";
 import newsreduce from "./news-reducer";
 import messagereduce from "./message-reducer";
 import peoplereduce from "./people-reducer";
 import authreduce from "./auth-reducer";
 import thunkMiddleware from "redux-thunk";
+import { reducer as formReducer } from "redux-form";
 let reducers = combineReducers({
   ProfilePage: profilereduce,
   NewsPage: newsreduce,
   MessagePage: messagereduce,
   PeoplePage: peoplereduce,
   auth: authreduce,
+  form: formReducer,
 });
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
